adcf7607d663164757f3e8dc83802aa53663bc6e
Alexander Kallaway <kallaway@me.com>
2017-11-15 15:18:29 -0500

    add interfaces folder

---
+++ interfaces/shape/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type triangle struct {
+	height float64
+	base   float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+type shape interface {
+	getArea() float64
+}
+
+func main() {
+	tr := triangle{
+		height: 23,
+		base:   46,
+	}
+	sq := square{
+		sideLength: 44,
+	}
+
+	printArea(tr)
+	printArea(sq)
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
